9e7df67a4dc1258dd4faaeccb0255c9f5ebb4be9
Habiba-Ben-Nasr <bennasrhabiba@gmail.com>
2021-12-20 20:52:13 +0100

    define get one user , delete user and edite user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -35,7 +35,17 @@ exports.getUsers = async (req, res) => {
   } catch (error) {
     console.log("get users failed", error);
     res.status(402).json({ msg: "Fetch users failed" });
+
   }
+};
+
+/*************************** Get one user *********************/
+exports.getUser = (req, res) => {
+  const { _id } = req.params;
+
+  User.find({ _id })
+    .then((user) => res.status(200).json(user[0]))
+    .catch((err) => res.json(err));
 };
 
 /*************************** User Login *********************/
@@ -66,4 +76,31 @@ exports.userLogin = async (req, res) => {
     res.status(400).json({ errors: err });
   }
 };
+/***************************Edit User *********************/
 
+
+exports.editUser = async (req, res) => {
+  let { _id } = req.params;
+  try {
+    await User.findByIdAndUpdate({ _id }, { $set: { ...req.body } });
+    res.status(203).json({ msg: "User updated successfully" });
+  } catch (error) {
+    console.log("User update failed", error);
+    res.status(403).json({ msg: "User update failed" });
+  }
+};
+
+
+/***************************  Delete User *********************/
+
+exports.deleteUser = async (req, res) => {
+  const { _id } = req.params;
+
+  try {
+    await User.findByIdAndDelete({ _id });
+    res.status(200).json({ msg: "User deleted with success" });
+  } catch (error) {
+    console.error("User delete failed", error);
+    res.status(402).json({ msg: "User delete failed" });
+  }
+};
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,13 +5,24 @@ const {
   addUser,
   getUsers,
   userLogin,
+  getUser,
+  editUser,
+  deleteUser,
+
+  
 } = require("../controllers/user.controller");
 
 const Router = express.Router();
 
 Router.post("/addUser", addUser);
-Router.get("/getUser", getUsers);
+Router.get("/getUsers", getUsers);
 Router.post("/login", userLogin);
+Router.get("/:_id", getUser);
+Router.put("/:_id", editUser);
+Router.delete("/:_id", deleteUser);
+
+
+
 Router.get("/currentUser", isAuth(), (req, res) => res.send(req.user));
 
 module.exports = Router;
