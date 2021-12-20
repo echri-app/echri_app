3a126f74f2c6604779ae21ec3313ffe5df51060e
Mohamed Ali <medali.bnasr@gmail.com>
2021-12-20 12:25:07 +0100

    authentification

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const isAuth = require("../middlewares/passport-setup");
 
 const {
   addUser,
@@ -11,5 +12,6 @@ const Router = express.Router();
 Router.post("/addUser", addUser);
 Router.get("/getUser", getUsers);
 Router.post("/login", userLogin);
+Router.get("/currentUser", isAuth(), (req, res) => res.send(req.user));
 
 module.exports = Router;
